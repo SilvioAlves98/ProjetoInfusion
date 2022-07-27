5778ac41e68a54ad16205c314ca378972a07030d
SilvioAlves98 <silvioluizmilanialves@gmail.com>
2022-07-27 00:57:52 -0300

    Arquivos do projeto

---
+++ js/main.js
@@ -0,0 +1,13 @@
+(function(){
+    var $body = document.querySelector('body');
+    $body.classList.remove('no-js');
+    $body.classList.add('js');
+
+    var menu = new Menu({
+        container: '.header__nav',
+        toggleBtn: '.header__btnMenu',
+        widthEnabled: 1024
+    })
+    
+
+})()
