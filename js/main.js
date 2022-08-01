868c4c6567c53a0ad94f4c825eff076e7f4440b4
SilvioAlves98 <silvioluizmilanialves@gmail.com>
2022-08-01 08:33:09 -0300

    Atualizações finais do projeto 01/08/2022

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 (function(){
     var $body = document.querySelector('body');
-    $body.classList.remove('no-js');
-    $body.classList.add('js');
+    $body.classList.remove('no-js')
+    $body.classList.add('js')
 
     var menu = new Menu({
         container: '.header__nav',
         toggleBtn: '.header__btnMenu',
-        widthEnabled: 1025,
+        widthEnable: 1024
+    })
+
+    var carouselImgs = new Carousel({
+        container: '.laptop_slider .slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next',
+    })
+
+    var carouselQuiotes = new Carousel({
+        container: '.quiote_slideshow',
+        itens: 'figure',
+        btnPrev: '.prev',
+        btnNext: '.next',
     })
 })()
--- js/menu.js
+++ js/menu.js
@@ -1,71 +1,68 @@
 function Menu(config){
-
-     this.nav = (typeof config.container ==='string') ?
-     document.querySelector(config.container): config.container
-
-     this.btn = (typeof config.toggleBtn ==='string') ?
-     document.querySelector(config.toggleBtn): config.toggleBtn 
-
-     this.maxWidth = config.widthEnabled || false;
-
-     var _opened = false;
-     var _this = this;
-
-     this.btn.removeAttribute('style')
-    
-     
-     if(this.maxWidth){
-        window.addEventListener('resize', e => {
-            if(window.innerWidth > _this.maxWidth){
-                _this.nav.removeAttribute('style')
-                _opened = true;
-            } else if(!this.nav.getAttribute('style')){
-                closeMenu();
-            }
-        })
-
-        if(window.innerWidth <= _this.maxWidth){
-            closeMenu();
-        }
-     }
-
-     this.btn.addEventListener('click', openOrClose);
-     
-
-     function openOrClose(){
+    this.nav = ( typeof config.container === 'string') ?
+    document.querySelector(config.container) : config.container
+ 
+    this.btn = ( typeof config.toggleBtn === 'string') ?
+    document.querySelector(config.toggleBtn) : config.toggleBtn
+ 
+    this.maxWidth = config.widthEnable || false;
+ 
+    var _opened = false;
+    var _this = this;
+ 
+    this.btn.removeAttribute('style')
+ 
+    if(this.maxWidth){
+       window.addEventListener('resize', e => {
+        if(window.innerWidth > _this.maxWidth){
+            _this.nav.removeAttribute('style')
+            _opened = true;
+         } else if(!this.nav.getAttribute('style'))
+           closeMenu()
+       })
+ 
+       if(window.innerWidth <= _this.maxWidth){
+           closeMenu()
+       }
+    }
+ 
+    this.btn.addEventListener('click', open0rClose )
+ 
+    function open0rClose() {
         if(!_opened){
-            openMenu();
+            openMenu()
         } else{
-            closeMenu();
+            closeMenu()
         }
-
-        function openMenu(){
-            var _top = _this.nav.getBoundingClientRect().top + 'px'
-
-            var _style = {
-                maxHeight: 'calc(100vh - '+ _top +')',
-                overflow: 'hidden'
-            }
-
-            applyStyleToNav(_style)
-
-           _opened = true; 
+    }
+ 
+    function openMenu(){
+        var _top = _this.nav.getBoundingClientRect().top + 'px'
+ 
+        var _style = {
+            maxHeight: 'calc(100vh - '+ _top +' )',
+            overflow: 'hidden'
         }
-
-        function applyStyleToNav(_style){
-           Object.keys(_style).forEach( stl => {
+ 
+        applyStyletoNav(_style)
+ 
+        _opened = true;
+    }
+ 
+    function  applyStyletoNav(_style){
+        Object.keys(_style).forEach(stl => {
             _this.nav.style[stl] = _style[stl]
-           }) 
+        })
+    }
+ 
+    function closeMenu(){
+        var _style = {
+            maxHeight: '0px',
+            overflow: 'hidden'
         }
-
-        function closeMenu(){
-            var _style = {
-                maxHeight: '0px',
-                overflow: 'hidden'
-            }
-
-            applyStyleToNav(_style)
-            _opened = false;
-        }
-     }
+ 
+        applyStyletoNav(_style)
+ 
+        _opened = false;
+    }
 }
